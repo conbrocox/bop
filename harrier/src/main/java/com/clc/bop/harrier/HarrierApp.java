ead84a0c3af347647ab40da163876cff96d4ac09
Conor Cox <conbrocox@gmail.com>
2021-10-29 16:59:27 -0600

    Can grab configurations

--- harrier/src/main/java/com/clc/bop/harrier/HarrierApp.java
+++ harrier/src/main/java/com/clc/bop/harrier/HarrierApp.java
@@ -5,10 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HarrierApp {
-
 	public static void main(String[] args) {
-		System.out.println("HELLO WORLD");
 		SpringApplication.run(HarrierApp.class, args);
 	}
-
 }
--- harrier/src/main/java/com/clc/bop/harrier/HarrierAppController.java
+++ harrier/src/main/java/com/clc/bop/harrier/HarrierAppController.java
@@ -1,12 +1,29 @@
 package com.clc.bop.harrier;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HarrierAppController {
-    @GetMapping("/foo") String foo() {
-        System.out.println("you called foo");
-        return "you called foo";
+    @Value("${um.ok}")
+    private String greeting;
+
+    @Value("${msg}")
+    private String somethingElse;
+
+    @GetMapping("/greeting")
+    public String getGreeting() {
+        return greeting;
+    }
+
+    @GetMapping("/somethingElse")
+    public String getSomethingElse() {
+        return somethingElse;
     }
 }
